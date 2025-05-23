fd8c10fe4426fa928b6f57eda607b6a0e9bb252b
Brianiac1 <twypresale@proton.me>
2025-05-23 13:29:01 -0400

    Final Empire deploy with fixed token image

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: 'black', color: 'gold', minHeight: '100vh', textAlign: 'center', padding: 40 }}>
+      <img src="/token.png" alt="$TWY" style={{ width: 150, marginBottom: 30 }} />
+      <h1>$TWY Web3 Empire</h1>
+      <p>Say hello to The World, Chico... and Everything in it.</p>
+      <button>Connect Wallet</button>
+    </div>
+  );
+}
